5699176896a37ce370df104623262880e3bd86da
Arash <arash.shakery@gmail.com>
2020-03-26 21:30:06 +0430

    feat: listen on all keys

--- src/index.js
+++ src/index.js
@@ -13,12 +13,14 @@ export default function createStore({
   store: storeType = 'ls',
   ...storeOptions
 } = {}) {
+  const allListeners = [];
   const listeners = {};
   function notify(changes) {
     const { key } = changes;
     if (listeners[key]) {
       listeners[key].forEach(handler => handler(changes));
     }
+    allListeners.forEach(handler => handler(changes));
   }
 
   const store = createTtlStore({
@@ -37,6 +39,12 @@ export default function createStore({
       store.destroy();
     },
     on(key, handler) {
+      if (typeof key === 'function') {
+        // listen for all keys
+        allListeners.push(key);
+        return () => this.off(key);
+      }
+
       if (!listeners[key]) {
         listeners[key] = [handler];
       } else {
@@ -51,8 +59,14 @@ export default function createStore({
         // remove listeners for all keys
         Object.keys(listeners).keys.forEach(this.off);
       } else if (!handler) {
-        // remove all listeners for a specific key
-        delete listeners[key];
+        if (typeof key === 'function') {
+          // remove listener of all keys
+          const ind = allListeners.indexOf(key);
+          if (ind >= 0) allListeners.splice(ind, 1);
+        } else {
+          // remove all listeners for a specific key
+          delete listeners[key];
+        }
       } else if (!listeners[key]) {
         // remove a specific listener for a specific key
         listeners[key] = listeners[key].filter(x => x !== handler);
